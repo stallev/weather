8d22b62ebdee06e8ce7c9097c1f2cf8404835739
Alexander <stallev@yahoo.com>
2022-03-01 17:41:22 +0300

    added main components naming

---
+++ src/common/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/container.scss';
+
+const Header = () => {
+  return (
+    <div className="container">
+      <p>Header</p>
+    </div>
+  )
+}
+
+export default Header;
